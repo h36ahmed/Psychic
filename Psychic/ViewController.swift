f1473a4fc63040240ed3f02335e7e0cd9a7ab5b4
Hassan <h36ahmed@gmail.com>
2017-12-24 16:15:06 -0500

    Adds Watch Extension

--- Psychic/ViewController.swift
+++ Psychic/ViewController.swift
@@ -9,8 +9,10 @@
 import UIKit
 import GameplayKit
 import AVFoundation
+import WatchConnectivity
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, WCSessionDelegate {
 
     @IBOutlet weak var cardContainerView: UIView!
     
@@ -18,9 +20,18 @@ class ViewController: UIViewController {
     
     var allCards = [CardViewController]()
     
+    var lastMessage: CFAbsoluteTime = 0
+    
     @IBOutlet weak var gradientView: GradientView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if (WCSession.isSupported()) {
+            let session = WCSession.default
+            session.delegate = self
+            session.activate()
+        }
         
         createParticles()
         loadCards()
@@ -38,6 +49,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let instructions = "Please ensure your Apple Watch is configured correctly. On your iPhone, launch Apple's 'Watch' configuration app then choose General > Wake Screen. On that screen, please disable Wake Screen On Wrist Raise, then select Wake For 70 Seconds. On your Apple Watch, please swipe up on your watch face and enable Silent Mode. You're done!"
+        let ac = UIAlertController(title: "Adjust your settings", message: instructions, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "I'm Ready", style: .default))
+        present(ac, animated: true)
+    }
+    
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesMoved(touches, with: event)
         
@@ -51,6 +71,9 @@ class ViewController: UIViewController {
                         card.frontImageView.image = UIImage(named: "cardStar")
                         card.isCorrect = true
                     }
+                }
+                if card.isCorrect {
+                    sendWatchMessage()
                 }
             }
         }
@@ -164,6 +187,37 @@ class ViewController: UIViewController {
         }
         
         perform(#selector(loadCards), with: nil, afterDelay: 2)
+    }
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        
+    }
+    
+    func sessionDidBecomeInactive(_ session: WCSession) {
+        
+    }
+    
+    func sessionDidDeactivate(_ session: WCSession) {
+        
+    }
+
+    func sendWatchMessage() {
+        let currentTime = CFAbsoluteTimeGetCurrent()
+        
+        // if less than half a second has passed, bail out
+        if lastMessage + 0.5 > currentTime {
+            return
+        }
+        
+        // send a message to the watch if it's reachable
+        if (WCSession.default.isReachable) {
+            // this is a meaningless message, but it's enough for our purposes
+            let message = ["Message": "Hello"]
+            WCSession.default.sendMessage(message, replyHandler: nil)
+        }
+        
+        // update our rate limiting property
+        lastMessage = CFAbsoluteTimeGetCurrent()
     }
 
 }
